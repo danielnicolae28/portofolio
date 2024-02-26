3762b641a9373e4f9d40b4980a6277f0c4912d35
danielnicolae28 <danielnicolae286@gmail.com>
2024-02-26 21:38:21 +0200

    experience tab update

--- src/components/ExperienceTab.tsx
+++ src/components/ExperienceTab.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
 
 const ExperienceTab = () => {
+
+  const tools:string[] = ['React','Node.JS','C','SQL'] 
+
   return (
-    <div>ExperienceTab</div>
+    <>
+<div className='flex m-10'>
+
+<div className='border-r mr-3'>
+  <img src="../../public/svg.svg" className='m-3 w-80	 ' alt="" />
+</div>
+
+<div>
+
+<h1 className='m-4 text-3xl'>Mes Engineer</h1>
+<p className='m-4 text-lg'>July 2022 - Present</p>
+<p className='m-4 text-lg'>
+  I am a MES engineer with focus on debugging, find new solution for the current application we maintain and develop custom procedure on data base and creating reporting tools.
+</p>
+
+<p className='text-lg m-4'>Development tools: {tools.map((tool)=><span key={tool} className='m-2 border p-1 border-cyan-400 bg-cyan-400'>{tool}</span>)}</p>
+
+
+</div>
+
+
+</div>
+
+
+
+    </>
   )
 }
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,22 +1,29 @@
 
 const Skills = () => {
 
-  const frontEndSkills:string[] =['HTML','CSS',"JavaScript","MaterialUI","Tailwind",'Bootstrap',"SASS",'Responsive design']
+  const frontEndSkillsJS:string[] =['Single-page application(SPA)','ES6','React','TypeScript','Redux']
+  const frontEndSkillsCSS:string[] =['HTML','CSS',"MaterialUI","Tailwind",'Bootstrap',"SASS",'Responsive design']
 
-  const backEndSkills:string[] = ['NodeJS','Express.JS','PosgrteSQL','SQL','PL/SQL','MongoDB']
+  const backEndSkills:string[] = ['NodeJS','Express.JS','PostgreSQL','SQL','PL/SQL','MongoDB']
 
   return (
     <>
 
 <div className="flex flex-col">
    
-   <div className="m-3 flex">
+   <div className="m-3 flex ">
     <h2 className="border-r mr-2 p-1 border-cyan-700">Front-end</h2>
-    <div className="grid grid-cols-4">
-
-    {frontEndSkills.map((skill)=>(<p className="m-2">{skill}</p>))}
-    </div>
+<div>
+    <h1 className="text-xl font-bold m-2 ">JavaScript</h1>
+    <ul className="grid grid-cols-4">
+    {frontEndSkillsJS.map((skill)=>(<li className="m-2">{skill}</li>))}
+    </ul>
+<h1 className="text-xl font-bold m-2 ">CSS</h1>
+    <ul  className="grid grid-cols-4">
+    {frontEndSkillsCSS.map((skill)=>(<li className="m-2"> {skill}</li>))}
+    </ul>
    </div>
+</div>
    <div className="m-3 flex">
    <h2  className="border-r mr-2 p-1 border-cyan-700">Back-end</h2>
    <div className="grid grid-cols-4">
