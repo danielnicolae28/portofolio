4edb35e8882c6c6c5127afa038f619d4da81120e
danielnicolae28 <danielnicolae286@gmail.com>
2024-03-07 20:30:26 +0200

    experience add

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonTypes } from "../../models/types"
 
 const Button = ({children,onClick}:ButtonTypes) => {
   return (
-    <button className="m-4 w-10 h-10 text-primary-4" onClick={onClick}>{children}</button>
+    <button className="m-4 w-10 h-10 text-primary-4 border-b" onClick={onClick}>{children}</button>
   )
 }
 
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -19,7 +19,7 @@ setSelected(false)
     
     
     <div className="py-20 w-full h-screen bg-slate-500  flex justify-center ">
-      <div className=" flex flex-col items-center justify-start bg-slate-50 w-1/2 h-1/2 absolute top-1/4 ">
+      <div className=" flex flex-col items-center justify-start bg-slate-50 md:w-1/2 md:h-1/2 mdabsolute top-1/4 ">
         <div>
 
     <Button onClick={selectionHandlerSklill}>Skills</Button>
