061f9b53f8b5f7ccaeb1e3c08f0b33de86d9e94d
danielnicolae28 <danielnicolae286@gmail.com>
2024-03-17 20:15:26 +0200

    work

---
+++ src/components/ui/WorkProject.tsx
@@ -0,0 +1,16 @@
+
+const WorkProject = () => {
+  return (
+    <>
+    
+    <div className="border bg-primary-4 w-96 h-60 flex justify-center items-center m-10">
+        <h1>Project Name</h1>
+
+    </div>
+
+
+    </>
+  )
+}
+
+export default WorkProject
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,10 +1,16 @@
 
+import WorkProject from "../components/ui/WorkProject"
+
 const Work = () => {
   return (
     <>
     
-    <div className="py-20 --color-4">
-    <h1>Projects</h1>
+    <div className="py-20 --color-4 flex w-full h-full flex-col justify-center items-center">
+    <h1 className="text-3xl font-semibold ">Projects</h1>
+    <WorkProject />
+    <WorkProject />
+    <WorkProject />
+    <WorkProject />
     </div>    
     
     </>
