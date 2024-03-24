f3a6db1c81d746bbccff26056c7658249db3578d
danielnicolae28 <danielnicolae286@gmail.com>
2024-03-24 21:17:57 +0200

    update

--- client/src/components/ui/WorkProject.tsx
+++ client/src/components/ui/WorkProject.tsx
@@ -2,11 +2,16 @@
 const WorkProject = () => {
   return (
     <>
-    
-    <div className="border bg-primary-4 w-96 h-60 flex justify-center items-center m-10 hover:blur-sm cursor-pointer">
-        <h1>Project Name</h1>
+  <div className="w-96 h-60 m-10 relative">
+<div  className="absolute flex flex-col items-center">
 
+        <h1 className=" bg-primary-1 p-5" >Project Name</h1>
+    <div className=" w-96 h-60  bg-primary-4  hover:blur-sm cursor-pointer">
+
+ 
     </div>
+</div>
+  </div>
 
 
     </>
